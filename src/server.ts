f34162fc556e73fbee01d2fa7937cf5257779119
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-20 21:52:32 +0100

    Add a blockchain to the server app, and a /transactions resource for displaying the transactions in the blockchain

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import { BlockChain } from './blockchain';
 
 const app = express();
+const blockchain = new BlockChain();
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/transactions', (req, res) => res.send(JSON.stringify(blockchain.all())));
 
 app.listen(8080, () => console.log('App started on port 8080'));
