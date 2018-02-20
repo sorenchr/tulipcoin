82025823ec585119889dc35010d63dfcc89b1680
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-20 22:22:32 +0100

    Add a port argument to the server script

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import * as express from 'express';
+import * as minimist from 'minimist';
 import { BlockChain } from './blockchain';
 
+// Parse arguments
+let args = minimist(process.argv.slice(2));
+let port = args.p || 8080;
+
+// Setup environment
 const app = express();
 const blockchain = new BlockChain();
 
-app.get('/transactions', (req, res) => res.send(JSON.stringify(blockchain.all())));
+app.get('/transactions', (req, res) => res.send('list all transactions'));
 
-app.listen(8080, () => console.log('App started on port 8080'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
