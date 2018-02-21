50a142bc9aef5cfa41a240b0d94b9aef20b3578d
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-21 19:15:09 +0100

    Add a 'Transfer coins' option to the client

--- src/client.ts
+++ src/client.ts
@@ -8,7 +8,7 @@ let args = minimist(process.argv.slice(2));
 if (!args.host) exitWithMessage('--host argument is required');
 if (!args.keys) exitWithMessage('--keys argument is required.');
 if (!args.type) exitWithMessage('--type arguments is required');
-if (!['create'].includes(args.type)) exitWithMessage('Unsupported type argument (create is supported)');
+if (!['create', 'transfer'].includes(args.type)) exitWithMessage('Unsupported type argument (create, transfer is supported)');
 
 // Setup environment
 const keys = JSON.parse(fs.readFileSync(args.keys, 'utf8'));
@@ -26,6 +26,21 @@ if (args.type === 'create') {
     restClient.createCoins(amount, to)
         .then(() => console.log(`${amount} coins were created succesfully`))
         .catch(err => console.log(`An error occurred while attempting to create coins: ${err}`));
+}
+
+// Check if coins should be transferred
+if (args.type === 'transfer') {
+    // Parse additional arguments
+    if (!args._[0]) exitWithMessage('Amount required as a parameter when transferring coins');
+    let amount = Number.parseInt(args._[0]);
+    if (amount == NaN) exitWithMessage('Amount is not recognized as a valid number');
+    if (!args._[1]) exitWithMessage('Receiver required as a parameter when transferring coins');
+    let to = args._[1];
+
+    // Send a "Transfer coins" transaction to the server
+    restClient.transferCoins(amount, keys.public, to)
+        .then(() => console.log(`${amount} coins were transferred succesfully`))
+        .catch(err => console.log(`An error occurred while attempting to transfer coins: ${err}`));
 }
 
 /**
--- src/restclient.ts
+++ src/restclient.ts
@@ -17,21 +17,26 @@ export class RestClient {
      */
     createCoins(amount: Number, to: string): Promise<string> {
         const body = { amount: amount, to: to };
-        return this.postRequest(body);
+        return this.request(body, '/coins', 'POST');
+    }
+
+    transferCoins(amount: Number, from: string, to: string): Promise<string> {
+        const body = { amount: amount, from: from, to: to };
+        return this.request(body, '/coins', 'PUT');
     }
 
     /**
-     * Executes a transaction POST request towards the server indicated by the user.
+     * Executes a HTTP request towards the server indicated by the user.
      * @param body The body that will be sent to the server.
      */
-    private postRequest(body: Object): Promise<string> {
+    private request(body: Object, path: string, method: string): Promise<string> {
         const bodyStringified = JSON.stringify(body);
 
         const options = {
             hostname: this.host,
             port: this.port,
-            path: '/transactions',
-            method: 'POST',
+            path: path,
+            method: method,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Content-Length': Buffer.byteLength(bodyStringified)
@@ -43,7 +48,10 @@ export class RestClient {
             const req = http.request(options, res => {
                 res.setEncoding('utf8')
                 res.on('data', chunk => resData += chunk);
-                res.on('end', () => resolve(resData));
+                res.on('end', () => {
+                    if (res.statusCode !== 200) return reject(resData);
+                    resolve(resData);
+                });
             });
             req.on('error', err => reject(err));
             req.write(bodyStringified);
