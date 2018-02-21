51fc92df5be598b91d44a7acd64fe9b6627b8ab1
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-21 12:57:06 +0100

    Make the restserver use a little router module for dispatching incoming requests to functions. Update the server module to handle create coin requests

--- src/restserver.ts
+++ src/restserver.ts
@@ -1,15 +1,52 @@
 import * as http from 'http';
+import { CreateCoins } from './transactions';
+import * as url from 'url';
 
 export class RestServer {
     server: http.Server;
+    createCoinsCb: (req: CreateCoins) => any;
 
     constructor(port: Number) {
-        this.server = http.createServer(this.requestHandler);
+        let router = new Router();
+        router.addRoute('/transactions', 'POST', this.postTransaction);
+        this.server = http.createServer(router.route);
         this.server.listen(port, () => console.log(`Server started on port ${port}`));
     }
 
-    requestHandler(req, res) {
-        console.log(req.url);
-        res.end('test');
+    private postTransaction(req: http.IncomingMessage, res: http.ServerResponse) {
+        let body = [];
+        req.on('data', (chunk) => body.push(chunk));
+        req.on('end', () => {
+            let bodyString = Buffer.concat(body).toString('utf8');
+            console.log(JSON.parse(bodyString));
+            res.end();
+        });
+    }
+
+    onCreateCoins(cb: (req: CreateCoins) => any) {
+        this.createCoinsCb = cb;
+    }
+}
+
+class Router {
+    private routes: { }
+    
+    constructor() {
+        this.routes = { };
+    }
+
+    route(req: http.IncomingMessage, res: http.ServerResponse) {
+        let urlParts = url.parse(req.url);
+        if (!(urlParts.path in this.routes)) return this.error(req, res);
+        this.routes[urlParts.path](req, res);
+    }
+
+    addRoute(path: string, method: string, handler: (req: http.IncomingMessage, res: http.ServerResponse) => any) {
+        if (!(path in this.routes)) this.routes[path] = {};
+        this.routes[path][method] = handler;
+    }
+
+    error(req: http.IncomingMessage, res: http.ServerResponse) {
+
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,15 @@ const port = args.p || 8080;
 // Setup environment
 const blockchain = new BlockChain();
 const restServer = new RestServer(port);
+
+// Handle created coins
+restServer.onCreateCoins(req => log(`${req.amount} new coins created for ${req.to}`));
+
+/**
+ * Logs a message to the console with a timestamp.
+ * @param msg The message to log.
+ */
+function log(msg) {
+    let timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    console.log(`[${timestamp}] ${msg}`);
+}
