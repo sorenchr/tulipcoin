bd94fcbddd86051a4a35607d22d8316c68f4d17d
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-02-21 09:00:40 +0100

    Add a keychain module that lets you generate 256-bit  public/private keypairs through the commandline, and optionally save the pair to disk

---
+++ src/keychain.ts
@@ -0,0 +1,17 @@
+import * as keypair from 'keypair';
+import * as minimist from 'minimist';
+import * as fs from 'fs';
+
+// Parse arguments
+let args = minimist(process.argv.slice(2));
+
+// Generate keys
+const keys = keypair({ bits: 256 });
+
+// Check if the keys should be saved to a file
+if (!!args.o) fs.writeFileSync(args.o, JSON.stringify(keys), 'utf8');
+
+// Display the keys to the user
+console.log(keys);
+
+
