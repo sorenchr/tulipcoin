c27f18f59b9fdcf3332483634a380ca7388e791e
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-04-12 16:18:41 +0200

    The blockchain will now set the id and prevTxId properties of new transactions that are appended

--- src/server/blockchain.ts
+++ src/server/blockchain.ts
@@ -1,4 +1,7 @@
 import { Transaction } from '../transaction';
+import { Logger } from '../logger';
+
+const logger = new Logger('blockchain');
 
 export class BlockChain {
     private transactions: Array<Transaction>;
@@ -8,7 +11,11 @@ export class BlockChain {
     }
 
     append(tx: Transaction): void {
+        let head = this.transactions.length > 0 ? this.transactions[this.transactions.length - 1] : null;
+        tx.id = head !== null ? head.id++ : 0;
+        if (head !== null) tx.prevTxId = head.id;
         this.transactions.push(tx);
+        logger.info(`Appending transaction with id: ${tx.id} to blockchain.`);
     }
 
     all(): Array<Transaction> {
