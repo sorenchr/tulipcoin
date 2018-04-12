0222e774db1f7095107cfe052c154773af6481d6
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-04-12 16:20:07 +0200

    Add a getWallet() method to the restclient and update postTransaction to use the new transaction format

--- src/client/restclient.ts
+++ src/client/restclient.ts
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import * as querystring from 'querystring';
-import { Transaction } from '../transaction';
+import { Transaction, Input, Output } from '../transaction';
+import { UTXO } from '../server/utxo';
 
 export class RestClient {
     host: string;
@@ -19,9 +20,9 @@ export class RestClient {
      * @param to The public key that the coins are transferred to.
      * @param prevTx The previous transaction that contains the coins needed for this transaction.
      */
-    transferCoins(amount: Number, from: string, to: string): Promise<string> {
-        let tx = new Transaction(to, from, amount, 0);
-        let body = JSON.stringify(tx.toJSON());
+    postTransaction(inputs: Array<Input>, outputs: Array<Output>): Promise<string> {
+        let tx = new Transaction(inputs, outputs);
+        let body = JSON.stringify(tx);
 
         const options = {
             hostname: this.host,
@@ -47,5 +48,27 @@ export class RestClient {
         });
     }
 
-    
+    /**
+     * Retrieve a list of unspent coins for the given wallet.
+     * @param publicKey The public key of the wallet.
+     */
+    getWallet(publicKey: string): Promise<Array<{txId: number, outputIndex: number, amount: number}>> {
+        const options = {
+            hostname: this.host,
+            port: this.port,
+            path: '/wallets/' + publicKey,
+            method: 'GET'
+        };
+
+        return new Promise((resolve, reject) => {
+            let resData = '';
+            const req = http.request(options, res => {
+                res.setEncoding('utf8')
+                res.on('data', chunk => resData += chunk);
+                res.on('end', () => res.statusCode === 200 ? resolve(JSON.parse(resData)) : reject(resData));
+            });
+            req.on('error', err => reject(err));
+            req.end();
+        });
+    }
 }
