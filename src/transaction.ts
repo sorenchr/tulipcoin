0d1ec7dc991289d576346c032e44a4c9fd399be9
Søren Qvist Christensen <soren.qvist@gmail.com>
2018-04-18 11:03:52 +0200

    Setup transactions to be signable and verifiable

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,23 +1,43 @@
+import * as NodeRSA from 'node-rsa';
+
 export class Transaction {
     id?: number;
     inputs: Array<Input>;
     outputs: Array<Output>;
     prevTxId?: number;
+    signature?: string;
+    publicKey: string;
 
-    constructor(inputs: Array<Input>, outputs: Array<Output>, prevTxId?: number, id?: number) {
+    constructor(inputs: Array<Input>, outputs: Array<Output>, publicKey: string, 
+                signature?: string, prevTxId?: number, id?: number) {
         this.inputs = inputs;
         this.outputs = outputs;
+        this.publicKey = publicKey;
+        this.signature = signature;
         this.prevTxId = prevTxId;
         this.id = id;
     }
 
-    toJSONString(): any {
+    toJSONString(): string {
         return JSON.stringify(this);
+    }
+
+    sign(privateKey: string) {
+        let key = new NodeRSA(privateKey);
+        let obj = { inputs: this.inputs, outputs: this.outputs };
+        this.signature = key.sign(JSON.stringify(obj), 'hex', 'utf8');
+    }
+
+    verifySignature() {
+        let key = new NodeRSA(this.publicKey);
+        let obj = { inputs: this.inputs, outputs: this.outputs };
+        return key.verify(JSON.stringify(obj), this.signature, 'utf8', 'hex');
     }
 
     static fromJSONString(jsonString): Transaction {
         let json = JSON.parse(jsonString);
-        return new Transaction(json.inputs, json.outputs, json.prevTxId, json.id);
+        return new Transaction(json.inputs, json.outputs, json.publicKey, json.signature,
+            json.prevTxId, json.id);
     }
 }
 
